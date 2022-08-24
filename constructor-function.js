809869b642f8016b12add04835ac491bc4826f7c
OsmanRana <osmancmc@yahoo.com>
2022-08-24 18:08:56 +0600

    class added

--- constructor-function.js
+++ constructor-function.js
@@ -1,5 +1,5 @@
 // Costructor Function
-function Person (firstName, lastName, dob){
+/* function Person (firstName, lastName, dob){
     this.firstName = firstName;
     this.lastName = lastName;
     this.dob = new Date(dob);
@@ -9,16 +9,33 @@ function Person (firstName, lastName, dob){
     // this.fullName = function (){
     //     return `${this.firstName} ${this.lastName}`
     // }
-}
+} */
 
-Person.prototype.getBirthYear = function (){
+/* Person.prototype.getBirthYear = function (){
     return this.dob.getFullYear();
 }
 
 Person.prototype.fullName = function (){
     return `${this.firstName} ${this.lastName}`
+} */
+
+// Class
+
+class Person {
+    constructor(firstName, lastName, dob){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = new Date(dob);
+
+    }
+    getBirthYear(){
+        return this.dob.getFullYear();
+    }
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
 }
 
 const person1 = new Person("Md Osman", "Farooque", "7-02-1983")
 console.log(person1.getBirthYear())
-console.log(person1.fullName())
+console.log(person1.getFullName())
