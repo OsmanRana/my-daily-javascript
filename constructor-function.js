7d5229856f592a1c2a8997ea62693c466900f6a0
OsmanRana <osmancmc@yahoo.com>
2022-08-24 18:01:58 +0600

    prototype added

--- constructor-function.js
+++ constructor-function.js
@@ -3,12 +3,20 @@ function Person (firstName, lastName, dob){
     this.firstName = firstName;
     this.lastName = lastName;
     this.dob = new Date(dob);
-    this.getBirthYear = function (){
-        return this.dob.getFullYear()
-    }
-    this.fullName = function (){
-        return `${this.firstName} ${this.lastName}`
-    }
+    // this.getBirthYear = function (){
+    //     return this.dob.getFullYear()
+    // }
+    // this.fullName = function (){
+    //     return `${this.firstName} ${this.lastName}`
+    // }
+}
+
+Person.prototype.getBirthYear = function (){
+    return this.dob.getFullYear();
+}
+
+Person.prototype.fullName = function (){
+    return `${this.firstName} ${this.lastName}`
 }
 
 const person1 = new Person("Md Osman", "Farooque", "7-02-1983")
